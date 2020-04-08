49dc1200396091ca62ba8cd0460a189379aa0456
JesseAbram <jesseabramowitz@hotmail.com>
2020-04-08 09:55:13 -0400

    rinkeby updates

--- src/mapping.ts
+++ src/mapping.ts
@@ -2,12 +2,12 @@ import { AnswerUpdated } from '../generated/chainlink Aggregators/chainlinkAggre
 import { updatedAnswer } from '../generated/schema'
 
 export function handleAnswerUpdated(event: AnswerUpdated): void {
-  let id = event.address.toHexString().concat("-").concat(event.params.answerId.toString())
+  let id = event.address.toHexString().concat("-").concat(event.params.roundId.toString())
   let updated = new updatedAnswer(id)
   updated.current = event.params.current
-  updated.answerId = event.params.answerId.toI32(),
+  updated.answerId = event.params.roundId.toI32(),
   updated.contract = event.address
-  updated.timestamp = event.block.timestamp.toI32(),
+  updated.timestamp = event.params.timestamp.toI32(),
 
   updated.save()
 }
